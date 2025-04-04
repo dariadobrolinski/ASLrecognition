d1f11463409a2c31d3e7ef94fcb92bb464e791b1
dariadobrolinski <dsdobrolinski@gmail.com>
2025-04-03 22:17:40 -0400

    Implement data collection and model training for hand landmark classification

--- create_dataset.py
+++ create_dataset.py
@@ -1,16 +1,54 @@
 import os
+import pickle
+
 import mediapipe as mp
 import cv2
 import matplotlib.pyplot as plt
 
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
 data_dir = './data'
 
+data = []
+labels = []
 for dir in os.listdir(data_dir):
-    for image_path in os.listdir(os.path.join(data_dir, dir))[:1]:
+    if dir.startswith("._"):
+        continue
+    for image_path in os.listdir(os.path.join(data_dir, dir)):
+        data_aux = []
+
+        x_ = []
+        y_ = []
+
         image = cv2.imread(os.path.join(data_dir, dir, image_path))
+        if image is None:
+            continue
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        plt.figure()
-        plt.imshow(image_rgb)
+        results = hands.process(image_rgb)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
 
-plt.show()
+                    x_.append(x)
+                    y_.append(y)
+
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x - min(x_))
+                    data_aux.append(y - min(y_))
+
+            data.append(data_aux)
+            labels.append(dir)
+
+f = open('data.pickle', 'wb')
+pickle.dump({'data': data, 'labels': labels}, f)
+f.close()

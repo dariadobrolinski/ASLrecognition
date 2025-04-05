2649f873184ac6053791e3fc2ac1676a78c7e49e
dariadobrolinski <dsdobrolinski@gmail.com>
2025-04-04 21:32:24 -0400

    fix: update .gitignore and improve text display in image collection and inference scripts

--- inference_classifier.py
+++ inference_classifier.py
@@ -68,7 +68,7 @@ while True:
 
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
             cv2.putText(frame, predicted_character, (x1, y1 - 20),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3, cv2.LINE_AA)
+                        cv2.FONT_HERSHEY_TRIPLEX, 1.3, (0, 0, 0), 3, cv2.LINE_AA)
 
     cv2.imshow('ASL Recognition', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
